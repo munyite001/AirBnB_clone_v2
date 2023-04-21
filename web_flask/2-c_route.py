3eceb77dd2e509fe20c374726e5542095e599e87
munyite001 <emunyite@gmail.com>
2023-04-21 12:43:02 +0300

    c-route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Run flask Web app
+via 0.0.0.0:5000
+Route '/' displays "Hello HBNB!"
+Route '/hbnb' displays "HBNB"
+Route 'c/<text>' displays C <text>
+
+"""
+from flask import Flask, Markup
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def display_text(text):
+    n = text.replace('_', ' ')
+    return f"C {Markup.escape(n)}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
