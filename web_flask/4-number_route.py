fc4ddaa7f72192a981831977d86c0b0c5fb457b7
munyite001 <emunyite@gmail.com>
2023-04-21 13:40:11 +0300

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Run flask Web app
+via 0.0.0.0:5000
+Route '/' displays "Hello HBNB!"
+Route '/hbnb' displays "HBNB"
+Route '/c/<text>' displays C <text>
+Route '/python/<text>' displays "python <text>"
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_route():
+    """returns Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """prints C followed by <text> content"""
+    text = text.replace("_", " ")
+    return "C %s" % text
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def py_text(text="is cool"):
+    """prints python followed by <text> content"""
+    text = text.replace("_", " ")
+    return "Python %s" % text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    if isinstance(n, int):
+        return '%d is a number' % n
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
