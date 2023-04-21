6dcb7718ed73e9e439fef6f3e18e779d2411a9f6
munyite001 <emunyite@gmail.com>
2023-04-21 13:32:29 +0300

    python is really cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -29,6 +29,7 @@ def c_text(text):
     text = text.replace("_", " ")
     return "C %s" % text
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<string:text>', strict_slashes=False)
 def py_text(text="is cool"):
